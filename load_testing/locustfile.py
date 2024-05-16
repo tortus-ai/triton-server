2c068749cfdffa9e6985c494947c8d03eca6247f
Nina Montana-Brown <56116848+NMontanaBrown@users.noreply.github.com>
2024-05-16 15:41:24 +0100

    Os 380 add load testing script for triton (#3)
    * OS-380 defined utility functions to parse model protobuf files into python objects
    
    * OS-380 defined base class to handle model configurations for load testing  and their data inputs.
    
    * OS-380 patch bug on data checking
    
    * OS-380 defined data parsing for fulfilling request body
    
    * OS-380 formatted method for data parsing for requests
    
    * OS-380 added data validation function to ensure correct types are enforced
    
    * OS-380 added Locust style object for load testing
    
    * OS-380 patched bugs with batching, string parsing logic, and string typing.
    
    * OS-380 refactored original base class into an HTTPUser
    
    * OS-380 update README with instructions on how to use load test
    
    * OS-380 added example data structure for llama load testing

---
+++ load_testing/locustfile.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+
+"""
+Locust file for load testing
+"""
+
+import os
+import json
+from locust import HttpUser, task, tag, constant_throughput, events
+from request_schema import \
+    parse_pbtxt_to_dict, \
+    convert_input_schema_into_request_data_dict, \
+    parse_data_for_request, \
+    validate_request_data_against_schema
+
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument("--schema", type=str, env_var="SCHEMA_PATH", help="Path to locust schema")
+    parser.add_argument("--authorization", env_var="AUTH_TOKEN",help="Bearer token")
+    parser.add_argument("--wait_time", type=float, env_var="WAIT_TIME", default=1)
+    parser.add_argument("--data", type=str, env_var="DATA_PATH", help="Path to data file")
+
+
+@events.test_start.add_listener
+def _(environment, **kw):
+    print(f"Custom argument supplied: {environment.parsed_options.data}")
+    print(f"Custom argument supplied: {environment.parsed_options.schema}")
+
+
+
+class LoadTest(HttpUser):
+    wait_time = constant_throughput(0.1)
+
+    def _read_env_vars(self):
+        """
+        Extends the LoadTestBase class to load test an endpoint.
+        """
+        kwargs = self.environment.parsed_options.__dict__
+        self.schema_path = kwargs.get("schema", os.environ.get("SCHEMA_PATH"))
+        self.host = kwargs.get("host", os.environ.get("HOST"))
+        self.authorization = kwargs.get("authorization", os.environ.get("AUTH_TOKEN"))
+        self.data_path = kwargs.get("data", os.environ.get("DATA_PATH"))
+        
+
+        # load the dictionary from the data string
+        assert os.path.exists(self.schema_path)
+        assert os.path.exists(self.data_path)
+        with open(self.data_path, 'r') as file:
+            data = json.load(file)
+        self.data = data
+
+    def parse_schema(self):
+        """
+        Convert the .pbtxt file for a given Triton model
+        into a dictionary.
+        """
+        self.schema = parse_pbtxt_to_dict(self.schema_path)
+
+    def get_request_input_data(self):
+        """
+        Method that parses the expected input dictionary
+        for the request from the model schema
+        """
+        self.input_data_body = convert_input_schema_into_request_data_dict(self.schema)
+
+    def verify_schema_inputs_against_data(self, data:dict):
+        """
+        For a given schema, verify that the provided
+        data dictionary is valid.
+        :param data: dict, data to be sent in the request
+        """
+        inputs = self.input_data_body["inputs"] # List of dicts
+        for input_dict in inputs:
+            assert input_dict["name"] in data.keys(), f"Input {input_dict['name']} not found in data dictionary"
+
+        self.data = data
+
+    def format_data(self):
+        """
+        Parse the data dictionary into a format that can be sent in the request. 
+        """
+        if not validate_request_data_against_schema(self.input_data_body, self.data):
+            raise ValueError("Data dictionary does not conform to schema")
+        
+        data_item = parse_data_for_request(self.data)
+        for sub_dict in self.input_data_body["inputs"]:
+            key = sub_dict["name"]
+            data = [data_item[key]]
+            sub_dict["data"].append(data)
+
+
+    @tag("inference")
+    @task
+    def predict(self):
+        headers = {
+            "Content-Type": "application/json"
+        }
+        self.client.post(self.host, json=self.input_data_body)
+
+    def on_start(self):
+        self._read_env_vars()
+        self.parse_schema()
+        self.get_request_input_data()
+        self.verify_schema_inputs_against_data(self.data)
+        self.format_data()
---
+++ load_testing/request_schema.py
@@ -0,0 +1,158 @@
+# coding=utf-8
+
+"""
+Utility functions to parse the model config
+into python objects that can be used to
+generate load tests
+"""
+
+import base64
+from pathlib import Path
+from typing import List
+import numpy as np
+
+def parse_pbtxt_to_dict(filepath):
+    """
+    This function takes a defined .pbtxt file and parses it into a dictionary.
+    This dictionary can be used to populate the load testing class automatically.
+    """
+    config = {}
+    current_list = None
+    current_dict = None
+
+    with open(filepath, 'r') as file:
+        for line in file:
+            stripped_line = line.strip()
+            if not stripped_line:
+                continue  # Skip empty lines
+            if stripped_line.endswith('['):
+                key = stripped_line.split('[')[0].strip()
+                config[key] = []
+                current_list = config[key]
+            elif stripped_line == ']':
+                current_list = None
+                current_dict = None
+            elif current_list is not None and stripped_line.endswith('{'):
+                current_dict = {}
+                current_list.append(current_dict)
+            elif current_dict is not None and stripped_line == '}':
+                current_dict = None
+            elif current_dict is not None:
+                if ':' in stripped_line:
+                    key, value = stripped_line.split(':', 1)
+                    key = key.strip()
+                    value = value.strip().strip('"')  # Remove any potential quotes
+                    if value.isdigit():  # Handle numeric values
+                        value = int(value)
+                    elif value.replace('.', '', 1).isdigit() and value.count('.') < 2:  # Handle float values
+                        value = float(value)
+                    current_dict[key] = value
+                else:
+                    continue  # Skip malformed lines within dictionaries
+            else:
+                if ':' in stripped_line:
+                    key, value = stripped_line.split(':', 1)
+                    key = key.strip()
+                    value = value.strip().strip('"')  # Remove any potential quotes
+                    if value.isdigit():  # Handle numeric values
+                        value = int(value)
+                    elif value.replace('.', '', 1).isdigit() and value.count('.') < 2:  # Handle float values
+                        value = float(value)
+                    config[key] = value
+                else:
+                    continue  # Skip malformed lines
+
+    return config
+
+def map_data_type_to_request_type(data_type:str):
+    """
+    Map the data type from the schema to the request type.
+    See: https://github.com/triton-inference-server/server/blob/main/docs/user_guide/model_configuration.md#datatypes
+    :param data_type: str, config data type
+    :return: str,
+    """
+    if data_type == "TYPE_STRING":
+        return "BYTES"
+    else:
+        return data_type.replace("TYPE_", "")
+
+def convert_input_schema_into_request_data_dict(schema:dict):
+    """
+    Parse the schema dictionary into a dictionary that can be used to send requests.
+
+    :param schema: dict,
+    :return: dict,
+    """
+    base_dict = {"inputs": []}
+    input_dict = schema["input"] # list of dictionaries
+    for input_item in input_dict:
+        input_data = {
+            "name": input_item["name"],
+            "shape": [1]+eval(input_item["dims"]), # converts string to list of ints
+            "datatype": map_data_type_to_request_type(input_item["data_type"]),
+            "data": []
+        }
+        base_dict["inputs"].append(input_data) 
+    return base_dict
+
+def parse_data_for_request(data:dict):
+    """
+    Parse the data dictionary into a format that can be sent in the request.
+    """
+    output_data = {}
+    for key, value in data.items():
+        if isinstance(value, str):
+            if Path(value).exists() and Path(value).extension in [".jpg", ".jpeg", ".png", ".bmp"]:
+                # If the value is a path to an image, convert to base64
+                with open(value, "rb") as f:
+                    image_base64 = base64.b64encode(f.read()).decode("utf-8")
+                    output_data[key] = image_base64
+            else:
+                output_data[key] = value
+        else:
+            output_data[key] = eval(value)
+    
+    return output_data
+
+def is_correct_type(value, type_str:str):
+    """
+    Check input data type against the expected type.
+    """
+    # Mapping of type strings to actual Python data types
+    type_mapping = {
+        "BOOL": bool,
+        "UINT8": np.uint8,
+        "UINT16": np.uint16,
+        "UINT32": np.uint32,
+        "UINT64": np.uint64,
+        "INT8": np.int8,
+        "INT16": np.int16,
+        "INT32": np.int32,
+        "INT64": np.int64,
+        "FLOAT16": np.float16,
+        "FLOAT": float,
+        "DOUBLE": float,
+        "BYTES": str
+    }
+
+    expected_type = type_mapping.get(type_str.strip().upper(), None)
+
+    if expected_type is None:
+        raise ValueError(f"Unsupported type string: {type_str}")
+
+    return isinstance(value, expected_type)
+
+def validate_request_data_against_schema(schema_dict:dict, data:dict):
+    """
+    Validate that the data dictionary conforms to the schema.
+    """
+    inputs = schema_dict["inputs"]
+    for input_dict in inputs:
+        name = input_dict["name"]
+        data_type = input_dict["datatype"]
+        value = data.get(name, None)
+        if value is None:
+            raise ValueError(f"Input {name} not found in data dictionary")
+        if not is_correct_type(value, data_type):
+            raise ValueError(f"Input {name} is not of the correct type {data_type}")
+    return True
