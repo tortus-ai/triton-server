730621a73115f1e0498d0d9128b97f912fa20439
Sal Khalil <sal.khalil@live.com>
2024-05-20 15:04:46 +0100

    fix: standardise exp file to run from root of repo

--- load_testing/experiments/experiment_1.sh
+++ load_testing/experiments/experiment_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-locust -f ../locustfile.py \
---data /Users/nin/repos/triton-sever/load_testing/example/data_llama_4000.json \
---schema /Users/nin/repos/triton-sever/model_repository/llama3_8b/config.pbtxt \
+locust -f ${PWD}/load_testing/locustfile.py \
+--data ${PWD}/load_testing/example/data_llama_4000.json \
+--schema ${PWD}/model_repository/llama3_8b/config.pbtxt \
 --host http://34.34.52.244/v2/models/llama3_8b/infer \
---loglevel DEBUG --run-time 60m
+--loglevel DEBUG --run-time 60m \
