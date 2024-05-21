e00eccb9cac9497b1e24c69ecdd4250a73bbcbed
Sal Khalil <sal.khalil@live.com>
2024-05-21 12:10:36 +0100

    chore: update exp script
    - fewer starting users
    - more rampage

--- load_testing/experiments/experiment_1.sh
+++ load_testing/experiments/experiment_1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 locust -f ${PWD}/load_testing/locustfile.py \
---data ${PWD}/load_testing/example/data_llama_4000.json \
---schema ${PWD}/model_repository/llama3_8b/config.pbtxt \
---host http://34.34.52.244/v2/models/llama3_8b/infer \
---loglevel DEBUG --run-time 60m \
+    --data ${PWD}/load_testing/data/soap_note.json \
+    --schema ${PWD}/model_repository/llama3_8b/config.pbtxt \
+    --host http://34.34.52.244/v2/models/llama3_8b/infer \
+    --loglevel DEBUG --run-time 60m \
+    --starting-users 10 --bulk-ramp 30
