bb1cf712b843a41a2a6ab3afa63b7ad859ff2199
Sal Khalil <sal.khalil@live.com>
2024-05-22 18:03:01 +0100

    feat: add exp result scripts

--- load_testing/experiments/2/run.sh
+++ load_testing/experiments/2/run.sh
@@ -5,4 +5,4 @@ locust -f ${PWD}/load_testing/locustfile.py \
     --schema ${PWD}/model_repository/llama3_8b/config.pbtxt \
     --host http://34.34.52.244/v2/models/llama3_8b/infer \
     --loglevel DEBUG --run-time 60m \
-    --starting-users 2 --bulk-ramp 2 --bulk-interval 60
+    --starting-users 16 --bulk-ramp 16 --bulk-interval 60
